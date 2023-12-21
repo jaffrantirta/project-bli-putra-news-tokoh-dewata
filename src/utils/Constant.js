ea27dd39f39bc136917c75e17811659b46ed97b4
Jaffran Tirta Artika <67328295+jaffrantirta@users.noreply.github.com>
2023-12-21 01:39:24 +0000

    fix

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,7 +1,9 @@
-export const APP_NAME = 'Aries Sujati'
-export const TAG_LINE = 'Gusti Ayu Aries Sujati'
-export const SUPABASE_URL = 'https://aunuzbrmdpmdsaekhtqa.supabase.co'
-export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1bnV6YnJtZHBtZHNhZWtodHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkwNTAzMjAsImV4cCI6MTk5NDYyNjMyMH0.JbGY4KvkImOGR98r61vtHf24xRo242m__zEypvNraeg'
-export const ERROR_MESSAGE = 'Ups! Sepertinya ada kesalahan'
-export const IS_LOGIN = localStorage.getItem('access_token') !== null ? true : false
-export const CACHE_EXPIRATION_TIME = 60000
+export const APP_NAME = "Made Kasta";
+export const TAG_LINE = "Madekasta";
+export const SUPABASE_URL = "https://pkucoytcpngrzezrrphy.supabase.co/";
+export const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrdWNveXRjcG5ncnplenJycGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5Njg4NzgsImV4cCI6MjAxODU0NDg3OH0.zI3VEWVSqOKW005KL2upDBNbnAGEfaSUxdAtV3t-pOQ";
+export const ERROR_MESSAGE = "Ups! Sepertinya ada kesalahan";
+export const IS_LOGIN =
+  localStorage.getItem("access_token") !== null ? true : false;
+export const CACHE_EXPIRATION_TIME = 60000;
