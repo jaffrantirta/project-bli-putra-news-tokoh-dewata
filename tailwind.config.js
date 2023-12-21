96ad171cf15f4770cb3a1d62a1a966758d3547fb
Jaffran Tirta Artika <67328295+jaffrantirta@users.noreply.github.com>
2023-12-21 07:58:32 +0000

    update color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,22 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        'primary': ['Fredoka', 'sans-serif'],
-        'secondary': ['Zeyada', 'cursive'],
-        'third': ['Dancing Script', 'cursive'],
+        primary: ["Fredoka", "sans-serif"],
+        secondary: ["Zeyada", "cursive"],
+        third: ["Dancing Script", "cursive"],
       },
       colors: {
-        primary: '#de0705',
-        accentColor: '#fadbc9',
-        secondary: '#fdf0da',
-        third: '#e9b250'
-      }
+        primary: "#87cefa",
+        accentColor: "#fadbc9",
+        secondary: "#fdf0da",
+        third: "#e9b250",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
