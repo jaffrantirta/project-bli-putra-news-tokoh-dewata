e6f42a5e9a8dee715e07b85835d23c70fb10ed23
jaffrantirta <jaffrantirta@icloud.com>
2023-12-27 16:56:48 +0800

    update name

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "Wayan Geredeg";
-export const TAG_LINE = "Wayan Geredeg";
+export const TAG_LINE = "I Wayan Geredeg.SH";
 export const SUPABASE_URL = "https://pxvxdxdwyfrqibwggwqe.supabase.co/";
 export const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dnhkeGR3eWZycWlid2dnd3FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM2MDYwMjgsImV4cCI6MjAxOTE4MjAyOH0.TMXFq-ywJfzj-qXv7utGOCUT2hGTywPLXWDPkxhGIms";
