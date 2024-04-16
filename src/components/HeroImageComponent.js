ee457a1457bf067361a10a36c2b4e9c59043129e
Jaffran Tirta Artika <67328295+jaffrantirta@users.noreply.github.com>
2024-04-16 12:45:45 +0000

    ok

--- src/components/HeroImageComponent.js
+++ src/components/HeroImageComponent.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import convertText from '../utils/ConvertText'
+import React from "react";
+import { Link } from "react-router-dom";
+import convertText from "../utils/ConvertText";
 
 export default function HeroImageComponent({ img, category, title, date, id }) {
-    return (
-        <Link to={`/read?id=${id}&news=${convertText(title)}`} className='relative cursor-pointer'>
-            <img src={img} alt={title} className="w-full h-full object-cover object-center aspect-video" />
-            <div className="absolute bottom-0 left-0 p-4 text-white">
-                <p className="bg-primary w-fit p-2 text-sm rounded-full text-white font-bold">{category}</p>
-                <p className="text-md md:text-3xl font-bold line-clamp-2 hover:text-primary">{title}</p>
-                <p className='text-sm'>{date}</p>
-            </div>
-        </Link>
-    )
+  return (
+    <Link
+      to={`/read?id=${id}&news=${convertText(title)}`}
+      className="relative cursor-pointer overflow-hidden"
+    >
+      <img
+        src={img}
+        alt={title}
+        className="w-full h-full object-cover object-center aspect-video transition-transform duration-300 transform scale-125 hover:scale-150 "
+      />
+      <div className="absolute bottom-0 left-0 p-4 text-white bg-gradient-to-t from-black w-full">
+        <p className="bg-primary w-fit p-2 text-sm rounded-full text-white font-bold">
+          {category}
+        </p>
+        <p className="text-md md:text-3xl font-bold line-clamp-2 hover:text-primary">
+          {title}
+        </p>
+        <p className="text-sm">{date}</p>
+      </div>
+    </Link>
+  );
 }
